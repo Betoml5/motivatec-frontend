29551eca791d87dde16ce03461e4d2d9be8c0126
Betoml5 <betoml5@hotmail.com>
2023-07-09 12:55:11 -0600

    Dummy change

--- src/screens/forum/Create.jsx
+++ src/screens/forum/Create.jsx
@@ -2,8 +2,11 @@ import { useMutation } from "react-query";
 import { createPostAPI } from "../../services/post";
 import { toast } from "react-toastify";
 
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Create = () => {
   const navigate = useNavigate();
@@ -20,9 +23,9 @@ const Create = () => {
   });
 
   const {
-    register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm();
 
   const onSubmit = (post) => {
@@ -34,36 +37,20 @@ const Create = () => {
   };
 
   return (
-    <div className="max-w-6xl mx-auto p-4">
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
-        <h1 className="text-2xl ">Crear Post</h1>
-        <input
-          className="input"
-          type="text"
-          name="title"
-          placeholder="Titulo"
-          {...register("title", { required: true })}
-        ></input>
-        {errors.title && (
-          <span className="text-red-500 text-sm">
-            El titulo no puede estar vacio
-          </span>
-        )}
-        <textarea
-          placeholder="Yo pienso que..."
+    <div className="max-w-6xl mx-auto my-2 bg-white">
+      
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
           name="content"
-          id="content"
-          cols="30"
-          rows="20"
-          className="input"
-          {...register("content", { required: true })}
-        ></textarea>
-        {errors.content && (
-          <span className="text-red-500 text-sm">
-            El contenido no puede estar vacio
-          </span>
-        )}
-        <button className="btn">Crear</button>
+          control={control}
+          render={({ field }) => (
+            <ReactQuill
+              value={field.value}
+              onChange={field.onChange}
+              className="h-full"
+            />
+          )}
+        />
       </form>
     </div>
   );
