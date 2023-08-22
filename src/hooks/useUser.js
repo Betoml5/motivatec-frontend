367aa8d78f2e88d7394e8945b9b37626ea57866d
Betoml5 <betoml5@hotmail.com>
2023-08-21 18:13:38 -0600

    Add change password UI

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,10 +1,23 @@
 import { useContext, useEffect } from "react";
 import { UserContext } from "../context/UserContext";
-import { useQuery } from "react-query";
-import { getCurrentSession } from "../services/user";
+import { useMutation, useQuery } from "react-query";
+import { changePasswordAPI, getCurrentSession } from "../services/user";
+import { toast } from "react-toastify";
+
 function useUser() {
   const { user, setUser } = useContext(UserContext);
   const { data, error, isLoading } = useQuery("user", getCurrentSession);
+  const { mutate: changePassword, isLoading: isPasswordLoading } = useMutation(
+    changePasswordAPI,
+    {
+      onSuccess: () => {
+        toast.success("Contraseña cambiada con éxito");
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    }
+  );
 
   useEffect(() => {
     if (!isLoading && data) {
@@ -12,7 +25,15 @@ function useUser() {
     }
   }, [data, isLoading, setUser]);
 
-  return { user, setUser, data, error, isLoading };
+  return {
+    user,
+    setUser,
+    data,
+    error,
+    isLoading,
+    changePassword,
+    isPasswordLoading,
+  };
 }
 
 export default useUser;
